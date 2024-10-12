fa302552b999acba9f23fcf3df97cd2241bdb933
Dalveer Singh <singhdalveer247@gmail.com>
2024-10-12 20:36:35 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
-import os
+import ctypes
 
+from elevate import elevate
+import os
+import pyclamd
 import eel
 import platform
 import psutil
 import pyautogui
-import sys
+import sys, subprocess
+
+elevate()
 
 eel.init("web")
 
 
+# Connect to the ClamAV daemon
+def connect_to_clamd():
+    try:
+        cd = pyclamd.ClamdNetworkSocket()  # Use network socket connection
+        if cd.ping():
+            print("Connected to ClamAV daemon")
+            return cd
+        else:
+            print("Failed to connect to ClamAV daemon")
+            return None
+    except pyclamd.ConnectionError:
+        print("Could not connect to ClamAV daemon.")
+        return None
+
+
 #  Count the total number of files to be scanned
-def count_files_os_walk(type):
+def count_files_os_walk(typ):
     file_count = 0
-    partitions = []
-    if type == "quick":
+    global partitions
+    if typ == "quick":
         partitions = [
             "C:\\Windows\\System32",
             "C:\\Program Files",
@@ -21,12 +41,12 @@ def count_files_os_walk(type):
             os.path.expandvars("%USERPROFILE%\\AppData\\Local\\Temp"),
             os.path.expandvars("%USERPROFILE%\\AppData\\Roaming"),
         ]
-    elif type == "full":
+    elif typ == "full":
         all_partitions = psutil.disk_partitions()
         for part in all_partitions:
             partitions.append(part.device)
     else:
-        pass #Custom scan files list
+        pass  # Custom scan files list
 
     for partition in partitions:
         directory = partition.replace("\\", "/")
@@ -37,10 +57,50 @@ def count_files_os_walk(type):
     return file_count
 
 
+# Scan a directory
+def scan_directory(cd, directory_path):
+    for root, dirs, files in os.walk(directory_path):
+        for file in files:
+            file_path = fr"{os.path.join(root, file)}"
+            scan_file(cd, file_path)
+
+
+# Scan a single file
+def scan_file(cd, file_path):
+    try:
+        result = cd.scan_file(file_path)
+        if result is None:
+            print(f"{file_path} is clean")
+        else:
+            print(f"Virus found in {file_path}: {result}")
+    except Exception as e:
+        print(f"Error scanning file {file_path}: {e}")
+
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+
 @eel.expose
 def scan_files():
-    count_files_os_walk("quick")
+    print("Starting Service.....")
+    command = "net start clamd"
+    if is_admin():
+        result = (subprocess.run(command, capture_output=True, text=True))
+        print(result)
+    else:
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, ' '.join(sys.argv), None, 1)
+    clamd_instance = connect_to_clamd()
+    count_files_os_walk("full")
+    if clamd_instance:
+        for par in partitions:
+            scan_directory(clamd_instance, par)
 
+
+partitions = []
 
 screen_reso = pyautogui.size()
 
