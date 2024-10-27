87ab355d898ebc093ceba9b3c5d1f6c374032f34
Dalveer Singh <singhdalveer247@gmail.com>
2024-10-27 16:23:08 +0530

    connecting backend with new UI

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 import pyclamd
 import eel
@@ -5,11 +6,42 @@ import platform
 import psutil
 import pyautogui
 import sys
-from concurrent.futures import ThreadPoolExecutor
 from threading import Lock
-import asyncio, datetime
+from concurrent.futures import ThreadPoolExecutor
+import ctypes
+import pyclamd
+from win32security import GetFileSecurity, OWNER_SECURITY_INFORMATION, LookupAccountSid
 
-eel.init("web")
+
+# Connect to the ClamAV daemon
+def connect_to_guardium():
+    try:
+        cd = pyclamd.ClamdNetworkSocket()  # Use network socket connection
+        if cd.ping():
+            print("Connected to Guardium daemon")
+            return cd
+        else:
+            print("Failed to connect to Guardium daemon")
+            return None
+    except pyclamd.ConnectionError:
+        print("Could not connect to Guardium daemon.")
+        return None
+
+
+# Count the total files number in the directory
+def count_files_in_directory(directory):
+    file_count = 0
+    try:
+        for _, _, files in os.walk(directory):
+            file_count += len(files)
+    except Exception as e:
+        print(f"Error counting files in {directory}: {e}")
+    return file_count
+
+
+async def count_files_in_directory_async(directory):
+    loop = asyncio.get_event_loop()
+    return await loop.run_in_executor(None, count_files_in_directory, directory)
 
 
 class Anti:
@@ -17,107 +49,109 @@ class Anti:
         self.lock = Lock()
         self.partitions = []
 
-    # Connect to the ClamAV daemon
-    def connect_to_guardium(self):
-        try:
-            cd = pyclamd.ClamdNetworkSocket()  # Use network socket connection
-            if cd.ping():
-                print("Connected to Guardium daemon")
-                return cd
-            else:
-                print("Failed to connect to Guardium daemon")
-                return None
-        except pyclamd.ConnectionError:
-            print("Could not connect to Guardium daemon.")
-            return None
-
-    async def count_files_in_directory_async(self, directory):
-        loop = asyncio.get_event_loop()
-        return await loop.run_in_executor(None, self.count_files_in_directory, directory)
-
-    def count_files_in_directory(self, directory):
-        file_count = 0
-        try:
-            for _, _, files in os.walk(directory):
-                file_count += len(files)
-        except Exception as e:
-            print(f"Error counting files in {directory}: {e}")
-        return file_count
-
     #  Count the total number of files to be scanned
     async def count_files(self, typ):
-        file_count = 0
         if typ == "quick":
+            # self.partitions = [
+            #     "C:\\Windows\\System32",
+            #     "C:\\Program Files",
+            #     "C:\\Program Files (x86)",
+            #     os.path.expandvars("%USERPROFILE%\\AppData\\Local\\Temp"),
+            #     os.path.expandvars("%USERPROFILE%\\AppData\\Roaming"),
+            # ]
             self.partitions = [
-                "C:\\Windows\\System32",
-                "C:\\Program Files",
-                "C:\\Program Files (x86)",
-                os.path.expandvars("%USERPROFILE%\\AppData\\Local\\Temp"),
-                os.path.expandvars("%USERPROFILE%\\AppData\\Roaming"),
+                r"D:\Treasure of Nadia [FitGirl Repack]"
             ]
         elif typ == "full":
             all_partitions = psutil.disk_partitions()
             for part in all_partitions:
-                # print(part.device)
                 self.partitions.append(part.device)
-            print(self.partitions)
+            # print(self.partitions)
         else:
             pass
 
-        tasks = [self.count_files_in_directory_async(partition.replace("\\", "/")) for partition in self.partitions]
+        tasks = [
+            count_files_in_directory_async(partition.replace("\\", "/"))
+            for partition in self.partitions
+        ]
         results = await asyncio.gather(*tasks)
 
         file_count = sum(results)
         print(f"Total number of files: {file_count}")
+        eel.total_files(file_count)
         return file_count
 
-        # for partition in self.partitions:
-        #     # directory = partition.replace("\\", "/")
-        #
-        #     for root, dirs, files in os.walk(partition):
-        #         file_count += len(files)
-        #     print(f"Total number of files: {file_count}")
-        #     return file_count
-
-    # Scan a directory
+    # Scan directory for viruses
     def scan_directory(self, cd, directory_path):
-        with ThreadPoolExecutor(max_workers=(os.cpu_count() * 10)) as executor:  # Adjust max_workers based on your CPU
+        eel.remove_raw()
+        with ThreadPoolExecutor(
+            max_workers=os.cpu_count() * 10
+        ) as executor:  # Adjust max_workers based on your CPU
             for root, _, files in os.walk(directory_path):
                 for file in files:
                     file_path = os.path.join(root, file)
                     executor.submit(self.scan_a_file, cd, file_path)
-                    if file_path == r"C:\Windows\System32\Firewall.cpl":
-                        # print()
-                        break
-                break
 
     # Scan a single file
     def scan_a_file(self, cd, file_path):
-        try:
+        
 
+        try:
             with self.lock:
+                eel.current_file(file_path)
+                owner = self.get_file_owner(file_path)
+                print(owner,self.is_owner_trusted(owner))
+                if (owner and self.is_owner_trusted(owner)) or owner == "Administrators":
+                    print(f"Skipping {file_path}, owner {owner} is trusted.")
+                    return
+                # eel.current_file(
+                #     os.path.basename(file_path)
+                # )  # Change file name on Front-End
                 result = cd.scan_file(rf"{file_path}")
-            if result is None:
-                print(f"{file_path} is clean")
-            else:
-                print(f"Virus found in {file_path}: {result}")
+                if result is None:
+                    print(f"{file_path} is clean")
+                else:
+                    print(f"Virus found in {file_path}: {result}")
         except Exception as e:
-            # with self.lock:
-            #     print(f"Error scanning file {file_path}: {e}")
-            #     print(f"File causing issue: {file_path}")
             print(f"Error scanning file {file_path}: {e}")
 
+    # //////////////////////////////////
+    def get_file_owner(self, file_path):
+        """Get the owner of a file (Windows)"""
+        try:
+            sd = GetFileSecurity(file_path, OWNER_SECURITY_INFORMATION)
+            owner_sid = sd.GetSecurityDescriptorOwner()
+            name, domain, _ = LookupAccountSid(None, owner_sid)
+            print(name)
+            return name
+        except Exception as e:
+            print(f"Error getting owner of {file_path}: {e}")
+            return None
 
+    def is_owner_trusted(self, owner):
+        """Determine if a file owner is a system user (trusted)"""
+        # Consider 'SYSTEM', 'Administrator', or 'TrustedInstaller' as trusted
+        if owner in ["SYSTEM", "Administrators", "TrustedInstaller"]:
+            return True
+        else:
+            return False
+
+
+# //////////////////////////////////////
+
+
+# Function to start the scanning functionalities.
 @eel.expose
-def start_scan():
+def start_scan(typ):
     anti = Anti()
-    clamd_instance = anti.connect_to_guardium()
-    asyncio.run(anti.count_files("quick"))
+    clamd_instance = connect_to_guardium()
+    asyncio.run(anti.count_files(typ))
     if clamd_instance:
-        starting_time = datetime.datetime.now()
         for par in anti.partitions:
             anti.scan_directory(clamd_instance, par)
-        print(f"Total time: {datetime.datetime.now() - starting_time}")
+
+
+eel.init("web")  # initialize eel
 
 
 screen_reso = pyautogui.size()
--- web/script.js
+++ web/script.js
@@ -1,10 +1,14 @@
-const scanButton = document.getElementById("scanButton");
-const progressCircle = document.querySelector(".progress-circle");
-const progressFile = document.querySelector(".progress_file");
-// const progressFile = document.querySelector(".progress_file span")
-const scan = document.getElementById("progressValue");
-const raw = document.getElementById("raw");
-const actionList = document.querySelector(".action-list");
+// const scanButton = document.getElementById("scanButton");
+// const progressCircle = document.querySelector(".progress-circle");
+// const progressFile = document.querySelector(".progress_file");
+// const scan = document.getElementById("progressValue");
+// const raw = document.getElementById("raw");
+// const actionList = document.querySelector(".action-list");
+
+const currentFile = document.querySelector(".current-file-container .current-file")
+const spinner = document.querySelectorAll(".spinner")
+
+console.log(spinner)
 
 let total = 0;
 let current = 0;
@@ -12,65 +16,72 @@ let progress = 0;
 let isScanning = false;
 let intervalId;
 
-function startScan() {
+function startScan(type) {
   // Reset progress and animation
-  type = scan.textContent.split(' ')[0].toLowerCase()
-  scan.textContent = "Scanning";
-  raw.textContent = "Starting Scan...";
+  // type = scan.textContent.split(' ')[0].toLowerCase()
+  // scan.textContent = "Scanning";
+  currentFile.textContent = "Starting Scan...";
   isScanning = true;
 
   eel.start_scan(type);
   
   progress = 0;
-  // current = 0;
-  progressCircle.style.background = "conic-gradient(red 0deg, #333 0deg)";
+  // progressCircle.style.background = "conic-gradient(red 0deg, #333 0deg)";
 
   // Start the progress
   intervalId = setInterval(() => {
     progress = (current / total) * 100; // Increase progress
-    progressCircle.style.background = `conic-gradient(red ${progress}deg, #333 ${progress}deg)`;
+    // spinner.background = `conic-gradient(red ${progress}deg, #333 ${progress}deg)`;
+    // console.log(progress)
+    spinner[0].style.background = `conic-gradient(var(--blue) ${progress}deg, var(--black) ${progress}deg)`;
+    spinner[1].style.background = `conic-gradient(var(--red) ${progress}deg, var(--black) ${progress}deg)`;
+    spinner[2].style.background = `conic-gradient(var(--purple) ${progress}deg, var(--black) ${progress}deg)`;
 
     if (progress >= 360) {
       isScanning = false;
-      clearInterval(intervalId);
-      // alert("Scan Complete!");
       console.log("Scan complete");
+      clearInterval(intervalId);
     }
-  }, 0.05);
+  }, 1);
 }
 
-scanButton.addEventListener("click", startScan);
+// scanButton.addEventListener("click", startScan);
 
+// Change text of main scan button according to clicked type on the sidebar.
+// actionList.addEventListener("click", function (event) {
+//   if (isScanning) return
+//   const value = event.target.textContent;
+//   if (event.target.textContent.includes("\n")) return;
+//   scan.textContent = value
+// });
 
-actionList.addEventListener("click", function (event) {
-  if (isScanning) return
-  const value = event.target.textContent;
-  if (event.target.textContent.includes("\n")) return;
-  scan.textContent = value
-});
+// remove "Start Scan" span and insert span for inserting
 
-eel.expose(remove_raw);
-function remove_raw() {
-  raw.parentElement.removeChild(raw);
-  progressFile.insertAdjacentHTML("beforeend", '<span id="content"></span>');
+// eel.expose(remove_raw);
+// function remove_raw() {
+//   raw.parentElement.removeChild(raw);
+//   progressFile.insertAdjacentHTML("beforeend", '<span id="content"></span>');
+// }
+
+// get total number of files from the python file
+eel.expose(total_files);
+function total_files(num_of_files) {
+  total = num_of_files;
+  // total = 100;
+  console.log(total);
 }
 
+// show the current scanning file on the front-end
 eel.expose(current_file);
 function current_file(file_name) {
   current += 1;
   file = file_name;
-  const content = document.getElementById("content");
-  content.textContent = limitString(file_name, 35);
-  console.log(file_name);
+  currentFile.textContent = limitString(file_name, 40);
+  // console.log(currentFile)
+  // console.log(file_name);
 }
 
-eel.expose(total_files);
-function total_files(num_of_files) {
-  // total = num_of_files;
-  total = 100;
-  console.log(total);
-}
-
+// limiting the length the file path showing on the front-end
 function limitString(str, noOfChars) {
   if (str.length <= noOfChars) return str;
 
