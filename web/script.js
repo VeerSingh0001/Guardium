0e9753c87652e0128967f3eccd88b0af14ddd35b
Dalveer Singh <singhdalveer247@gmail.com>
2024-11-04 13:22:57 +0530

    added progress status

--- web/script.js
+++ web/script.js
@@ -1,53 +1,47 @@
 const currentFileContainer = document.querySelector(".current-file-container");
-const currentFile = document.querySelector(
-  ".current-file-container .current-file"
-);
+const currentFile = document.getElementById("file-name");
 const spinner = document.querySelectorAll(".spinner");
 const cancel = document.querySelector(".center button");
-const scanResult = document.getElementsByClassName("virus-detect")[0];
+// const scanResult = document.getElementsByClassName("virus-detect")[0];
 const swiperButtons = document.querySelectorAll(".swiper-btn");
+const currentProgress = document.getElementById("progress");
 
 let total = 0;
 let degreePerFile = 0;
 let progress = 0;
+let currentFileNumber = 0;
 let intervalId;
 let isScanning = false;
-let scanType = "quick"
+let scanType = "quick";
 
-function setNav(type, hide){
-    if (hide) {
-      // 
-      if (type == "quick"){
-        swiperButtons[1].classList.add('swiper-button-disabled')
-      }
-      else if (type == "full"){
-        swiperButtons[0].classList.add('swiper-button-disabled')
-        swiperButtons[1].classList.add('swiper-button-disabled')
-      }
-      else{
-        swiperButtons[0].classList.add('swiper-button-disabled')
-      }
+function setNav(type, hide) {
+  if (hide) {
+    //
+    if (type == "quick") {
+      swiperButtons[1].classList.add("swiper-button-disabled");
+    } else if (type == "full") {
+      swiperButtons[0].classList.add("swiper-button-disabled");
+      swiperButtons[1].classList.add("swiper-button-disabled");
     } else {
-      if (type == "quick"){
-        swiperButtons[1].classList.remove('swiper-button-disabled')
-      }
-      else if (type == "full"){
-        swiperButtons[0].classList.remove('swiper-button-disabled')
-        swiperButtons[1].classList.remove('swiper-button-disabled')
-      }
-      else{
-        swiperButtons[0].classList.remove('swiper-button-disabled')
-      }
+      swiperButtons[0].classList.add("swiper-button-disabled");
     }
-
+  } else {
+    if (type == "quick") {
+      swiperButtons[1].classList.remove("swiper-button-disabled");
+    } else if (type == "full") {
+      swiperButtons[0].classList.remove("swiper-button-disabled");
+      swiperButtons[1].classList.remove("swiper-button-disabled");
+    } else {
+      swiperButtons[0].classList.remove("swiper-button-disabled");
+    }
+  }
 }
-
 
 function startScan(type) {
   // Reset progress and animation
-  scanType = type
+  scanType = type;
   if (isScanning) return;
-  setNav(type, true)
+  setNav(type, true);
   // swiperNext.classList.add('swiper-button-disabled')
   // swiperPrev.classList.add('swiper-button-disabled')
 
@@ -73,13 +67,18 @@ function startScan(type) {
     // spinner[0].style.background = `conic-gradient(var(--blue) ${progress}deg, var(--black) ${progress}deg)`;
     // spinner[1].style.background = `conic-gradient(var(--red) ${progress}deg, var(--black) ${progress}deg)`;
     // spinner[2].style.background = `conic-gradient(var(--purple) ${progress}deg, var(--black) ${progress}deg)`;
+    const progressId = document.getElementById(`progress-${type}`);
+    progressId.classList.remove("hide")
+    progressId.textContent = `${
+      Math.round((currentFileNumber / total) * 100) || 0
+    }%`;
 
     if (progress > 360) {
       console.log("Scan complete");
       eel.show_result();
       clearInterval(intervalId);
       isScanning = false;
-      setNav(type, false)
+      setNav(type, false);
       // swiperNext.classList.remove('swiper-button-disabled')
       // swiperPrev.classList.remove('swiper-button-disabled')
     }
@@ -93,7 +92,7 @@ function cancelScan() {
   spinner[2].style.background = `conic-gradient(var(--purple) ${0}deg, var(--black) ${0}deg)`;
   // swiperNext.classList.remove('swiper-button-disabled')
   // swiperPrev.classList.remove('swiper-button-disabled')
-  setNav(scanType, false)
+  setNav(scanType, false);
   eel.cancel_scan(); // Call the Python cancel_scan method
   clearInterval(intervalId);
   currentFile.textContent = "Scanning Stopped";
@@ -113,6 +112,7 @@ function current_file(file_name) {
   if (currentFileContainer.classList.contains("hide")) {
     currentFileContainer.classList.remove("hide");
   }
+  currentFileNumber += 1;
   progress += degreePerFile; // Increase progress
   file = file_name;
   currentFile.textContent = limitString(file_name, 30);
@@ -128,42 +128,31 @@ function limitString(str, noOfChars) {
 // Show detected viruses
 eel.expose(showResult);
 function showResult(result) {
-  scanResult.innerHTML = "";
-  scanResult.insertAdjacentHTML(
-    "beforeend",
-    `<div class="threats">
-      <div class="row title">
-        <div class="col title">Threat</div>
-        <div class="col risk">Risk</div>
-        <div class="col actions">Action</div>
-      </div>
-      <div class="scrolls">
-      </div>
-    </div>`
-  );
-  const scrolls = document.getElementsByClassName("scrolls")[0];
-  if (result.length < 1) {
+  const scrolls = document.getElementsByClassName("scrolls")[0]
+  result.forEach((res) => {
     scrolls.insertAdjacentHTML(
       "beforeend",
-      `<div class="result">No virus founded</div>`
-    );
-  } else {
-    result.forEach((res) => {
-      scrolls.insertAdjacentHTML(
-        "beforeend",
-        `<div class="row">
+      `<div class="row">
           <div class="col title">${res["virus_name"]}</div>
-          <div class="col risk risk-${res["severity"].toLowerCase()}">${
-          res["severity"]
-        }</div>
+          <div class="col risk risk-${res["severity"].toLowerCase()}">${res["severity"]}</div>
           <div class="col actions">
-              <button class="btn" onclick="action('remove')">Remove</button>
-              <button class="btn" onclick="action('Quarantine')">Quarantine</button>
-              <button class="btn" onclick="action('allow')">Allow</button>
+            <button class="btn" onclick="action('remove')">Remove</button>
+            <button class="btn" onclick="action('quarantine')">Quarantine</button>
+            <button class="btn" onclick="action('allow')">Allow</button>
           </div>
-        </div>
-      `
-      );
-    });
-  }
+        </div>`
+      // `<div class="row">
+      //     <div class="col title">${res["virus_name"]}</div>
+      //     <div class="col risk risk-${res["severity"].toLowerCase()}">${
+      //   res["severity"]
+      // }</div>
+      //     <div class="col actions">
+      //         <button class="btn" onclick="action('remove')">Remove</button>
+      //         <button class="btn" onclick="action('Quarantine')">Quarantine</button>
+      //         <button class="btn" onclick="action('allow')">Allow</button>
+      //     </div>
+      //   </div>
+      // `
+    );
+  });
 }
