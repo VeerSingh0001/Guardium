dc27f2055056186f38ea11e93c630a8f7193d218
Dalveer Singh <singhdalveer247@gmail.com>
2024-11-04 12:47:18 +0530

    refined version of navigation ui

--- web/script.js
+++ web/script.js
@@ -1,45 +1,104 @@
-
-const currentFile = document.querySelector(".current-file-container .current-file");
+const currentFileContainer = document.querySelector(".current-file-container");
+const currentFile = document.querySelector(
+  ".current-file-container .current-file"
+);
 const spinner = document.querySelectorAll(".spinner");
 const cancel = document.querySelector(".center button");
-const scanResult = document.getElementsByClassName("virus-detect")[0]
-
+const scanResult = document.getElementsByClassName("virus-detect")[0];
+const swiperButtons = document.querySelectorAll(".swiper-btn");
 
 let total = 0;
 let degreePerFile = 0;
 let progress = 0;
-let intervalId
+let intervalId;
+let isScanning = false;
+let scanType = "quick"
+
+function setNav(type, hide){
+    if (hide) {
+      // 
+      if (type == "quick"){
+        swiperButtons[1].classList.add('swiper-button-disabled')
+      }
+      else if (type == "full"){
+        swiperButtons[0].classList.add('swiper-button-disabled')
+        swiperButtons[1].classList.add('swiper-button-disabled')
+      }
+      else{
+        swiperButtons[0].classList.add('swiper-button-disabled')
+      }
+    } else {
+      if (type == "quick"){
+        swiperButtons[1].classList.remove('swiper-button-disabled')
+      }
+      else if (type == "full"){
+        swiperButtons[0].classList.remove('swiper-button-disabled')
+        swiperButtons[1].classList.remove('swiper-button-disabled')
+      }
+      else{
+        swiperButtons[0].classList.remove('swiper-button-disabled')
+      }
+    }
+
+}
+
 
 function startScan(type) {
   // Reset progress and animation
+  scanType = type
+  if (isScanning) return;
+  setNav(type, true)
+  // swiperNext.classList.add('swiper-button-disabled')
+  // swiperPrev.classList.add('swiper-button-disabled')
+
+  isScanning = true;
   currentFile.textContent = "Starting Scan...";
+  const spinner = document.querySelector(
+    `.spinner${type === "quick" ? "" : `.${type}`}`
+  );
   isScanning = true;
 
   eel.run_scan(type);
 
+  const colors = {
+    quick: "--blue",
+    full: "--red",
+    custom: "--purple",
+  };
+
   progress = 0;
   // Start the progress
   intervalId = setInterval(() => {
-    spinner[0].style.background = `conic-gradient(var(--blue) ${progress}deg, var(--black) ${progress}deg)`;
-    spinner[1].style.background = `conic-gradient(var(--red) ${progress}deg, var(--black) ${progress}deg)`;
-    spinner[2].style.background = `conic-gradient(var(--purple) ${progress}deg, var(--black) ${progress}deg)`;
+    spinner.style.background = `conic-gradient(var(${colors[type]}) ${progress}deg, var(--black) ${progress}deg)`;
+    // spinner[0].style.background = `conic-gradient(var(--blue) ${progress}deg, var(--black) ${progress}deg)`;
+    // spinner[1].style.background = `conic-gradient(var(--red) ${progress}deg, var(--black) ${progress}deg)`;
+    // spinner[2].style.background = `conic-gradient(var(--purple) ${progress}deg, var(--black) ${progress}deg)`;
 
     if (progress > 360) {
       console.log("Scan complete");
       eel.show_result();
       clearInterval(intervalId);
+      isScanning = false;
+      setNav(type, false)
+      // swiperNext.classList.remove('swiper-button-disabled')
+      // swiperPrev.classList.remove('swiper-button-disabled')
     }
   }, 1);
 }
 
 function cancelScan() {
-  progress = 0
+  progress = 0;
+  spinner[0].style.background = `conic-gradient(var(--blue) ${0}deg, var(--black) ${0}deg)`;
+  spinner[1].style.background = `conic-gradient(var(--red) ${0}deg, var(--black) ${0}deg)`;
+  spinner[2].style.background = `conic-gradient(var(--purple) ${0}deg, var(--black) ${0}deg)`;
+  // swiperNext.classList.remove('swiper-button-disabled')
+  // swiperPrev.classList.remove('swiper-button-disabled')
+  setNav(scanType, false)
   eel.cancel_scan(); // Call the Python cancel_scan method
-  clearInterval(intervalId)
-  currentFile.textContent = "Scanning Stopped"
+  clearInterval(intervalId);
+  currentFile.textContent = "Scanning Stopped";
 }
 cancel.addEventListener("click", cancelScan);
-
 
 // Get total number of files from the python file
 eel.expose(total_files);
@@ -51,9 +110,12 @@ function total_files(num_of_files) {
 // Show the current scanning file on the front-end
 eel.expose(current_file);
 function current_file(file_name) {
+  if (currentFileContainer.classList.contains("hide")) {
+    currentFileContainer.classList.remove("hide");
+  }
   progress += degreePerFile; // Increase progress
   file = file_name;
-  currentFile.textContent = limitString(file_name, 40);
+  currentFile.textContent = limitString(file_name, 30);
 }
 
 // Limiting the length the file path showing on the front-end
@@ -63,11 +125,10 @@ function limitString(str, noOfChars) {
   return `${str.split("").slice(0, noOfChars).join("")}...`;
 }
 
-
 // Show detected viruses
 eel.expose(showResult);
 function showResult(result) {
-  scanResult.innerHTML = ''
+  scanResult.innerHTML = "";
   scanResult.insertAdjacentHTML(
     "beforeend",
     `<div class="threats">
@@ -106,5 +167,3 @@ function showResult(result) {
     });
   }
 }
-
-
